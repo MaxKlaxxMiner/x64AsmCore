9514929d3be1e589e943e5fa8bf31bdf966da963
MaxKlaxxMiner <max@tetrisk.de>
2017-10-13 01:03:32 +0200

    append first opcodes (00 00 - 00 03)

--- OpCodeGenerator/Program.cs
+++ OpCodeGenerator/Program.cs
@@ -1,26 +1,44 @@
-﻿using System;
+﻿#region # using *.*
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+#endregion
 
 namespace OpCodeGenerator
 {
   class Program
   {
+    /// <summary>
+    /// liest alle bekannten, von Hand geschriebenen OpCodes aus Textdateien herraus und gibt diese zurück
+    /// </summary>
+    /// <returns>Enumerable der Zeilen, welche gelesen wurden</returns>
     static IEnumerable<string> ReadKnownOpCodes()
     {
-      yield break;
+      foreach (var line in File.ReadLines("KnownOpCodes/0000-.txt")) if (!string.IsNullOrWhiteSpace(line) && line.Trim()[0] != '#') yield return line.Trim();
     }
 
+    /// <summary>
+    /// generiert alle OpCodes anhand bestimmter Regeln
+    /// </summary>
+    /// <returns>Enumerable der Zeilen, welche generiert wurden</returns>
     static IEnumerable<string> GenerateOpCodes()
     {
       yield break;
     }
+
+    /// <summary>
+    /// maximale Anzahl der Vorschau-Zeilen, falls eine Liste länger sein sollte
+    /// </summary>
+    const int SamplePreview = 10;
 
     static void Main(string[] args)
     {
       var known = ReadKnownOpCodes().ToArray();
       var gen = GenerateOpCodes().ToArray();
       int count = Math.Min(known.Length, gen.Length);
+
+      // --- bekannte OpCodes mit den generierten Version vergleichen und beim ersten Fehler stoppen
       for (int i = 0; i < count; i++)
       {
         Console.WriteLine("  ok: " + gen[i]);
@@ -34,9 +52,33 @@ namespace OpCodeGenerator
         }
       }
       Console.WriteLine();
-      if (known.Length < gen.Length) Console.WriteLine("Missing known-opcodes: {0:N0}", gen.Length - known.Length);
-      if (gen.Length < known.Length) Console.WriteLine("Missing generated-opcodes: {0:N0}", known.Length - gen.Length);
 
+      // --- Vorschau von eventuell Fehlenden OpCodes ---
+      if (known.Length < gen.Length)
+      {
+        Console.WriteLine("Missing known-opcodes: {0:N0}", gen.Length - known.Length);
+        Console.WriteLine();
+        for (int i = count; i < Math.Min(SamplePreview, known.Length); i++)
+        {
+          Console.WriteLine("  gen: " + known[i]);
+        }
+        if (known.Length > SamplePreview) Console.WriteLine("  gen: ...");
+        Console.WriteLine();
+      }
+      if (gen.Length < known.Length)
+      {
+        Console.WriteLine("Missing generated-opcodes: {0:N0}", known.Length - gen.Length);
+        Console.WriteLine();
+        for (int i = count; i < Math.Min(SamplePreview, known.Length); i++)
+        {
+          Console.WriteLine("  known: " + known[i]);
+        }
+        if (known.Length > SamplePreview) Console.WriteLine("  known: ...");
+        Console.WriteLine();
+      }
+
+      // --- auf Tastendruck warten, falls mit VS im Debug-Modus gestartet wurde (Fenster würde sich sonst sofort schließen) ---
+      Console.WriteLine();
       if (Environment.CommandLine.Contains(".vshost.exe"))
       {
         Console.Write("Press any key to continue . . . ");
