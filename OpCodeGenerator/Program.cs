6a3d9728e844df42ad0ccfc61b2d0561bf674f3a
MaxKlaxxMiner <max@tetrisk.de>
2017-10-13 00:50:40 +0200

    simple opcode comparer

--- OpCodeGenerator/Program.cs
+++ OpCodeGenerator/Program.cs
@@ -1,16 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace OpCodeGenerator
 {
   class Program
   {
+    static IEnumerable<string> ReadKnownOpCodes()
+    {
+      yield break;
+    }
+
+    static IEnumerable<string> GenerateOpCodes()
+    {
+      yield break;
+    }
+
     static void Main(string[] args)
     {
+      var known = ReadKnownOpCodes().ToArray();
+      var gen = GenerateOpCodes().ToArray();
+      int count = Math.Min(known.Length, gen.Length);
+      for (int i = 0; i < count; i++)
+      {
+        Console.WriteLine("  ok: " + gen[i]);
+        if (gen[i] != known[i])
+        {
+          Console.WriteLine();
+          Console.WriteLine("Different found:");
+          Console.WriteLine("  [known] " + known[i]);
+          Console.WriteLine("    [gen] " + gen[i]);
+          break;
+        }
+      }
+      Console.WriteLine();
+      if (known.Length < gen.Length) Console.WriteLine("Missing known-opcodes: {0:N0}", gen.Length - known.Length);
+      if (gen.Length < known.Length) Console.WriteLine("Missing generated-opcodes: {0:N0}", known.Length - gen.Length);
 
+      if (Environment.CommandLine.Contains(".vshost.exe"))
+      {
+        Console.Write("Press any key to continue . . . ");
+        Console.ReadKey(true);
+      }
     }
   }
 }
