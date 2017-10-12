d8d24fa8384f9d034cd178da78308ff91eadfbb3
MaxKlaxxMiner <max@tetrisk.de>
2017-10-13 00:39:16 +0200

    create project OpCodeGenerator

---
+++ OpCodeGenerator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpCodeGenerator
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+
+    }
+  }
+}
