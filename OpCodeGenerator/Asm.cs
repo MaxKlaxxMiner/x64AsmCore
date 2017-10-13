69a39a347230a3d5fa641bf9f1df18234cd78e0f
MaxKlaxxMiner <max@tetrisk.de>
2017-10-13 20:28:44 +0200

    generate opcodes (01 00 - 01 ff)

--- OpCodeGenerator/Asm.cs
+++ OpCodeGenerator/Asm.cs
@@ -8,17 +8,46 @@
     /// <summary>
     /// Namen der 64-Bit Basis-Register
     /// </summary>
-    public static readonly string[] RegistersR64 = 
+    public static readonly string[] RegistersR64 =
     {
-      "rax", "rcx", "rdx", "rbx", "rsp", "rbp", "rsi", "rdi",
-      "r8",  "r9",  "r10", "r11", "r12", "r13", "r14", "r15",
-      "",    "",    "",    "",    "",    "rip", "",    ""
+      "rax", "rcx", "rdx", "rbx",
+      "rsp", "rbp", "rsi", "rdi",
+      "r8",  "r9",  "r10", "r11",
+      "r12", "r13", "r14", "r15",
+      "",    "",    "",    "",
+      "",    "rip", "",    ""
+    };
+
+    /// <summary>
+    /// Namen der 32-Bit Basis-Register
+    /// </summary>
+    public static readonly string[] RegistersR32 =
+    {
+      "eax", "ecx", "edx", "ebx",
+      "esp", "ebp", "esi", "edi",
+      "",    "",    "",    "",
+      "",    "eip", "",    ""
+    };
+
+    /// <summary>
+    /// Namen der 16-Bit Basis-Register
+    /// </summary>
+    public static readonly string[] RegistersR16 =
+    {
+      "ax", "cx", "dx", "bx",
+      "sp", "bp", "si", "di",
+      "",   "",   "",   "",
+      "",   "ip", "",   ""
     };
 
     /// <summary>
     /// Namen der 8-Bit Basis-Register
     /// </summary>
-    public static readonly string[] RegistersR8 = { "al", "cl", "dl", "bl", "ah", "ch", "dh", "bh" };
+    public static readonly string[] RegistersR8 =
+    {
+      "al", "cl", "dl", "bl",
+      "ah", "ch", "dh", "bh"
+    };
 
     /// <summary>
     /// Namen der Assembler-Befehle
